4321bc259d628d12e30ebfd362fe70d5e689c7dd
skyrunner360 <mathurrishi31@gmail.com>
2024-11-17 00:19:46 +0530

    feat: :tada: Landing Page WIP

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: [
+      "uploadthing.com",
+      "utfs.io",
+      "img.clerk.com",
+      "subdomain",
+      "files.stripe.com",
+    ],
+  },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
