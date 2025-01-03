4b2a53ac677dfc7bbc4b3f5bd28ed68e6bf399b4
skyrunner360 <mathurrishi31@gmail.com>
2025-01-03 19:44:05 +0530

    feat: :sparkles: all subaccounts wip

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -371,3 +371,21 @@ export const changeUserPermissions = async (
     console.log("Could not change Permission", error);
   }
 };
+
+export const getSubaccountDetails = async (subaccountId: string) => {
+  const response = await db.subAccount.findUnique({
+    where: {
+      id: subaccountId,
+    },
+  });
+  return response;
+};
+
+export const deleteSubAccount = async (subaccountId: string) => {
+  const response = await db.subAccount.delete({
+    where: {
+      id: subaccountId,
+    },
+  });
+  return response;
+};
